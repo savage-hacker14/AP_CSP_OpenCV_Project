1890eae52ead4192a784a542154578bc8a71ff4e
Jacob Krucinski <jacob1576@gmail.com>
2018-12-03 10:01:18 -0500

    Got project set up at school. Started to implement car pipeline!

---
+++ OpenCV Java/src/CarDetection.java
@@ -0,0 +1,10 @@
+// Written by Jacob Krucinski on 12/03/18
+import carPipeline.CarPipeline;
+public class CarDetection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CarPipeline carDetector = new CarPipeline();
+	}
+
+}

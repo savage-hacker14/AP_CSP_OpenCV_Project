2b274a9625fbcc44810cbfd512ff1c11095e2472
Jacob Krucinski <jacob1576@gmail.com>
2018-12-04 20:04:08 -0500

    Worked on CarDetection (added my custom functions/methods)!

--- OpenCV Java/src/CarDetection.java
+++ OpenCV Java/src/CarDetection.java
@@ -21,6 +21,16 @@ import carPipeline.CarPipeline.Line;
 
 
 public class CarDetection {
+	// Static variables (used for distance calibration)
+	private static int pixelsToFt = (int)(130 / 16.1); 			// At approx. 45 ft
+	// Avg. car length = 16.1ft
+	// Avg car pixel length @ ~45ft: 130 pixels
+	private static double secToHr = 1 / 3600.0;
+	private static int miInFeet = 5280;
+	
+	// General variables
+	private static int speedLimit = 25; 	// [mph]
+	private static int frameRate = 30;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -36,7 +46,7 @@ public class CarDetection {
 		// 1: Raw camera feed
 		JFrame raw = new VideoFrame();
 		Container pane = raw.getContentPane();
-		raw.setTitle("Webcam Feed");
+		raw.setTitle("Raw Feed");
 		raw.setSize(665, 552);					// Possibly optimize this line
 		raw.setVisible(true);
 		raw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// allows program termination when x is clicked on
@@ -44,13 +54,19 @@ public class CarDetection {
 		// 2: Filtered camera feed
 		JFrame filtered = new VideoFrame();			// MODIFY THIS!!!
 		Container pane2 = raw.getContentPane();
-		filtered.setTitle("Webcam Feed");
+		filtered.setTitle("Filtered Feed");
 		filtered.setSize(665, 552);					// Possibly optimize this line
 		filtered.setVisible(true);
 		filtered.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		// allows program termination when x is clicked on
 
+//		// Debugging for my functions
+//		Line a = new Line(0, 10, 10, 20);
+//		Point midpoint = getLineMidPt(a);
+//		System.out.println("Midpoint of line a is " + midpoint);
+//		System.out.println("Speed: " + getCarSpeed(new Point(0, 50), new Point(8, 50)) + " mph");
 		
 		// Forever loop
+		Point prevCarMidPt = new Point(0, 0);
 		while (true) {
 			// Capture one frame from webcam
 			// (already done in VideoFrame class)
@@ -65,14 +81,56 @@ public class CarDetection {
 			
 			// Obtain desired output from carDetector
 			ArrayList<Line> lines = carDetector.filterLinesOutput();
-			
+			Line carLine = lines.get(0);		// Assuming there will be only one line in this ArrayList if processing is done well
+			Point carMidPt = getLineMidPt(carLine);
 			
 			// Display filter frame in new gui window
+			// Figure out on 12/5/18
+			
+			// Set prevCarMidPt to carMidPt
+			prevCarMidPt = carMidPt.clone();
 		}
 	}
 	
+	// My functions/algorithms for car speed detection
+	/**
+	 * 
+	 * @param line l
+	 * @return midpoint of line l (as object Point)
+	 */
 	public static Point getLineMidPt(Line l) {
+		int x1 = (int)l.x1;
+		int x2 = (int)l.x2;
+		int y1 = (int)l.y1;
+		int y2 = (int)l.y2;
 		
+		int avgX = (x1 + x2) / 2;
+		int avgY = (y1 + y2) / 2;
+		
+		return new Point(avgX, avgY);
+	}
+	
+	/**
+	 * 
+	 * @param a - midpoint of one car line
+	 * @param b - midpoint of 
+	 * @return speed of car [mph]
+	 */
+	public static double getCarSpeed(Point a, Point b) {
+		int aX = (int)a.x;
+		int bX = (int)b.x;
+		int xDiff = aX - bX;
+		
+		// NOTE: These are distance traveled in the duration of one frame to 
+		// another (1 / frameRate s, i.e 1/30 s)
+		int pixelDistance = Math.abs(xDiff);
+		double distanceFt  = (double)pixelDistance / pixelsToFt;
+		double distanceMi = distanceFt / miInFeet;
+		
+		double miPerS = distanceMi * frameRate;
+		double mph = miPerS * (1 / secToHr);
+		
+		return mph;
 	}
 
 }

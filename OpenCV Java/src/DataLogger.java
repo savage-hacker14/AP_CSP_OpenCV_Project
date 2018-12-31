cf7c7f7257cfe60cbd9e69e6907a78f189721505
Jacob Krucinski <jacob1576@gmail.com>
2018-12-31 15:35:25 -0500

    Commented DataLogger class!

--- OpenCV Java/src/DataLogger.java
+++ OpenCV Java/src/DataLogger.java
@@ -9,14 +9,15 @@ import java.util.ArrayList;
 
 public class DataLogger {
 	// Instance variables (variables to be logged)	
-	private ArrayList<String> times;		// Might come in as a long
+	private ArrayList<String> times;		
 	private ArrayList<Double> speeds;
 	private ArrayList<Boolean> isSpeeding;
-	private ArrayList<Color> colors;		// Might come in as a Color object, then convert to string
+	private ArrayList<Color> colors;	
 	
-	public DataLogger() {}
+	public DataLogger() {}	// Default constructor necessary but is never used
 	
 	public DataLogger(ArrayList<String> t, ArrayList<Double> s, ArrayList<Boolean> s2, ArrayList<Color> c) {
+		// Set all input parameters to this object's instance variables
 		times = t;
 		speeds = s;
 		isSpeeding = s2;
@@ -24,11 +25,11 @@ public class DataLogger {
 	}
 	
 	public void logData() throws IOException {
-		// Acquire date from system
+		// Acquire time date from system
 		LocalDateTime now = LocalDateTime.now();
 		String date = now.toString().substring(0, 10);
 		
-		// Create writer object
+		// Create writer object (with proper name and save location)
 		BufferedWriter writer = new BufferedWriter(new FileWriter("LogFiles/" + date + ".txt"));
 		
 		// Write header for .txt file
@@ -39,13 +40,21 @@ public class DataLogger {
 			writer.write(times.get(i) + "\t" + speeds.get(i) +"\t\t\t" + isSpeeding.get(i) + "\t\t\t\t" + colors.get(i) + "\n");
 		}
 		
+		// Calculate statistics for the logged data
 		calculateStats(writer);
 		
-		// Flush text from buffer and write it to th3w
+		// Flush text from buffer and write it to the file
 		writer.flush();
 		writer.close();
 	}
 	
+	/**
+	 * Writes the header for the text file
+	 * 
+	 * @param the file writer
+	 * @param the data (as a String object)
+	 * @throws IOException
+	 */
 	private void writeHeader(BufferedWriter writer, String date) throws IOException {	
 		String loggedVars = "Time [s]" + "\t\t" + "Speed [mph]" + "\t\t" + "Is Speeding?" + "\t\t" + "Color" + "\n";
 		String lineBreak = "------------------------------------------------------------------------------------\n";
@@ -55,6 +64,13 @@ public class DataLogger {
 		writer.write(lineBreak);	
 	}
 	
+	/**
+	 * This method takes the data from the instance variables and 
+	 * calculates useful/good-to-know stats about it
+	 * 
+	 * @param the file writer
+	 * @throws IOException
+	 */
 	private void calculateStats(BufferedWriter writer) throws IOException {
 		writer.write("\n");
 		writer.write("Compiled Statistics: \n");
@@ -62,6 +78,11 @@ public class DataLogger {
 		writer.write("Frames w/ Speeding Cars:\t" + numSpeedingCars() + "\n");
 	}
 	
+	/**
+	 * Calculates the average speed of all the speeds logged
+	 * 
+	 * @return the average speed rounded to 2 decimal places
+	 */
 	private double averageSpeed() {
 		double sumSpeeds = 0.0;
 		
@@ -75,6 +96,12 @@ public class DataLogger {
 		return avgSpeedRounded;
 	}
 	
+	/**
+	 * Finds the number of frames where the car was speeding
+	 * (num of instances of true in the isSpeeding list)
+	 * 
+	 * @return the number of frames where the car was speeding 
+	 */
 	private int numSpeedingCars() {
 		// Since the data logger is logging every frame (and not just one entry per car), 
 		// this will be the number of frames with speeding cars
